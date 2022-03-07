18328b30eebc84a22c90117d4717ffc83ec63321
Vladimir Solovyov <2728060+xpinguin@users.noreply.github.com>
2022-03-07 21:42:01 +0300

    SWI-cpp: likes.cpp reformatted for my convenience

--- likes.cpp
+++ likes.cpp
@@ -5,6 +5,7 @@
 
 #include "SWI-cpp.h"
 #include <iostream>
+
 using namespace std;
 
 /* Usage:
@@ -19,51 +20,52 @@ using namespace std;
    swipl-ld -o likes -ld g++ -goal true likes.cpp likes.pl
 */
 
-int
-body(int argc, char **argv)
-{ if ( argc == 1 )
-  { if ( strcmp(argv[0], "-happy") == 0 )
-    { PlTermv av(1);			/* likes - happy */
-
+int body(int argc, char **argv) {
+  if (argc == 1) {
+    if (strcmp(argv[0], "-happy") == 0) {
       cout << "Happy people:" << endl;
+
+      PlTermv av(1); /* likes - happy */
+
       PlQuery q("happy", av);
-      while( q.next_solution() )
-	cout << "\t" << (char *)av[0] << endl;
-    } else
-    { PlTermv av(2);
-
-      cout << argv[0] << " likes:" << endl;
-      av[0] = argv[0];
-      PlQuery q("likes", av);
-      while( q.next_solution() )
-	cout << "\t" << (char *)av[1] << endl;
+      while (q.next_solution()) {
+        cout << "\t" << (char *)av[0] << endl;
+      }
     }
-  } else if ( argc == 2 )
-  { PlTermv av(2);
+    else {
+      cout << argv[0] << " likes:" << endl;
 
+      PlTermv av(2);
+      av[0] = argv[0];
+
+      PlQuery q("likes", av);
+      while (q.next_solution()) {
+        cout << "\t" << (char *)av[1] << endl;
+      }
+    }
+  }
+  else if (argc == 2) {
+    PlTermv av(2);
     av[0] = argv[0];
     av[1] = argv[1];
-    if ( PlCall("likes", av) )
-      cout << "yes" << endl;
-    else
-      cout << "no" << endl;
-  } else
+
+    if (PlCall("likes", av)) cout << "yes" << endl;
+    else                                    cout << "no" << endl;
+  }
+  else {
     cout << "Usage: likes x [y] or likes -happy" << endl;
+  }
 
   return 0;
 }
 
-
-int
-main(int argc, char **argv)
-{ PlEngine e(argv[0]);
-
-  try
-  { return body(argc-1, argv+1);
-  } catch ( PlException &ex )
-  { cerr << (char *) ex << endl;
+int main(int argc, char **argv) {
+  PlEngine e(argv[0]);
+  try {
+    return body(argc - 1, argv + 1);
+  }
+  catch (PlException &ex) {
+    cerr << (char *)ex << endl;
     exit(1);
   }
 }
-
-
